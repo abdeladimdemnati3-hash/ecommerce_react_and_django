6f495b46d4457be01fc7da8b8be2123eb3ec6013
Abdeladim <abdeladimdemnati3@gmail.com>
2025-10-13 16:23:28 +0100

    feat: integrate axios for product fetching and add proxy configuration

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import { Row, Col, Container } from 'react-bootstrap';
 import Product from '../component/Product';
-import products from '../products'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios' 
 
 function HomeScreen() {
+   const [products, setProducts] = useState([])
+   
+   useEffect(() => {
+     async function fetchProducts() {
+      const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+     }
+     fetchProducts()
+     },[])
+
+
+
+     
   return (
     <Container className="py-5">
       <h1 className="text-center mb-4">Latest Products</h1>
@@ -11,7 +25,7 @@ function HomeScreen() {
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
-        ))}
+          ))}
       </Row>
     </Container>
   )
